069a78403e468594d5a05147f1f84f20ac14c6d0
david-d25@yandex.ru <david-d25@yandex.ru>
2021-03-31 10:31:55 +0300

    a

--- project/src/main/kotlin/space/davids_digital/CsvDumper.kt
+++ project/src/main/kotlin/space/davids_digital/CsvDumper.kt
@@ -2,6 +2,7 @@ package space.davids_digital
 
 import java.io.File
 import java.io.PrintWriter
+import kotlin.math.abs
 
 class CsvDumper {
     companion object {
@@ -10,12 +11,15 @@ class CsvDumper {
                 writer.println("x,f(x),sin(x),cos(x),tan(x),cot(x),sec(x),csc(x),log(x),log2(x),log3(x),log5(x),log10(x)")
                 var x: Double = from
                 while (x < to) {
-                    writer.print("$x, ${solver.solve(x)}, ${math.sin(x)}, ${math.cos(x)}, ${math.cot(x)}, ${math.csc(x)}, ")
+                    writer.print("$x, ${solver.solve(x)}, ${math.sin(x)}, ${math.cos(x)}, ${math.tan(x)}, ${math.cot(x)}, ${math.sec(x)}, ${math.csc(x)}, ")
                     writer.println(
                         if (x > 0)  "${math.log(x)}, ${math.log2(x)}, ${math.log3(x)}, ${math.log5(x)}, ${math.log10(x)}"
                         else        "NaN, NaN, NaN, NaN, NaN"
                     )
                     x += step
+
+                    if (abs(x) <= 1e-6)
+                        x = 0.0
                 }
             }
         }
@@ -24,5 +28,5 @@ class CsvDumper {
 
 fun main() {
     val math = ScrapMathImpl(BasicMath())
-    CsvDumper.dumpCsv(math, ScrapSolver(math), "file.csv", -1.0, 1.0, 0.1)
+    CsvDumper.dumpCsv(math, ScrapSolver(math), "file.csv", -2.0, 2.0, 0.1)
 }
